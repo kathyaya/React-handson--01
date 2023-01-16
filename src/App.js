028ac891a9294e2fc6cdf056cc19bc35ab49b969
kathyayanikr <kathyayanikr2112001@gmail.com>
2023-01-16 13:18:56 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Styling using Functional and Class Component </h1>
+
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css'
+import FunctionalComp from './components/FunctionalComp';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <>
+    <h1 className='title'>Styling Using Functional and Class Component</h1>
+
+    <FunctionalComp />
+
+
+
+  </>
+
+);
